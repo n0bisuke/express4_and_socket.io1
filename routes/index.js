05fa87e2c588db403482f4a51c533cc28bddb4cb
sugawara ryousuke <rsksound@gmail.com>
2014-11-28 00:10:06 +0900

    scaffold

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  res.render('d3', { title: 'Express' });
 });
 
 module.exports = router;
